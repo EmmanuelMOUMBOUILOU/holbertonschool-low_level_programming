0eb49167971c499958033f8f5705f0db406c2853
EmmanuelMOUMBOUILOU <emmanuelmoumbouilou@gmail.com>
2025-02-14 20:48:41 +0000

    modification of program in format betty

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
+int main(void)
+{
 
     printf("with proper grammar, but the outcome is a piece of art,\n");
 
-    return 0;
+    return (0) ;
 
 }
