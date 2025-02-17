f9510a00781ba17456caedd6e4fab4e23010d363
EmmanuelMOUMBOUILOU <emmanuelmoumbouilou@gmail.com>
2025-02-17 14:24:38 +0000

    modified program

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -10,11 +10,11 @@ int main(void)
 
 {
 
-	printf("Size of an int : %zu octets\n", sizeof(int));
-	printf("Size of a float : %zu octets\n", sizeof(float));
-	printf("Size of a long int : %zu octets\n", sizeof(long int));
-	printf("Size of a long long int : %zu octets\n", sizeof(long long int));
-	printf("Size of a char : %zu octets\n", sizeof(char));
+	printf("Size of an int : %zu byte(s)\n", sizeof(int));
+	printf("Size of a float : %zu byte(s)\n", sizeof(float));
+	printf("Size of a long int : %zu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int : %zu byte(s)\n", sizeof(long long int));
+	printf("Size of a char : %zu byte(s)\n", sizeof(char));
 
 	return (0);
 
