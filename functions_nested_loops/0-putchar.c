69f6731fc24c13ca7e25d52ca71a8103cf3754a5
EmmanuelMOUMBOUILOU <emmanuelmoumbouilou@gmail.com>
2025-02-19 21:22:00 +0000

    this program prints _putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
 
 /**
- * main -  prints _putchar, followed by a new line
- * Return: Always 0.
+ * main - prints the character
+ *
+ * Return: Always 0
  */
 int main(void)
 {
-	printf("_putchar\n");
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
 	return (0);
 }
