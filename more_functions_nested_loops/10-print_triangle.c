343eec9d08ff3e3d524d3efb1210255d0f9ac2e2
EmmanuelMOUMBOUILOU <emmanuelmoumbouilou@gmail.com>
2025-02-25 16:00:28 +0000

    prints a triangle, followed by a new line.

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_square -  prints a square, followed by a new line.
+ * @size: the integer to be c
+ *
+ * Return: Nothing
+ *
+ */
+
+void print_triangle(int size)
+{
+	int i;
+	int j;
+
+	for (i = 1; i <= size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			_putchar(' ');
+		}
+		for (k = 0; k < i; k++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+}
