25fd3181daf1c164692067f60bffa428bea1bfac
EmmanuelMOUMBOUILOU <emmanuelmoumbouilou@gmail.com>
2025-03-02 16:38:08 +0000

    compares two strings.

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strcmp - Compare two strings
+ * @s1: First string
+ * @s2: Second string
+ *
+ * Return: Negative value if s1 < s2, 0 if s1 == s2, positive value if s1 > s2
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+		i++;
+	}
+
+	return (s1[i] - s2[i]);
+}
+
