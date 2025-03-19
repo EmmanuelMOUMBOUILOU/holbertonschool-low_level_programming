5b287868c9cc222be4ab2efd53e9d4e61ea0f0ab
EmmanuelMOUMBOUILOU <emmanuelmoumbouilou@gmail.com>
2025-03-19 16:17:49 +0000

    prints anything

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ *  print_all - prints anything, followed by a new line.
+ * @format: the pointer
+ *
+ * Return: Nothing
+ */
+
+void print_all(const char * const format, ...)
+{
+	char c;
+	int i;
+	float f;
+	char *s;
+	int len;
+
+	va_list args;
+	va_start(args, format);
+	while (format[len])
+	{
+		if (format == 'c')
+		{
+			c = va_arg(args, char);
+			print("%c", c);
+		}
+		else if (format == 'i')
+		{
+			i = va_arg(args , int);
+			printf("%d", i);
+		}
+		else if (format == 'f')
+		{
+			f = va_arg(args, double);
+			printf("%f", f);
+		}
+		else if (format == 's')
+		{
+			s = va_arg(args, char *);
+			if (s == NULL)
+				printf("(nil)");
+			else
+				print("%s", s);
+		}
+		len++;
+	}
+	va_end(args);
+	printf("\n");
+}
