2a820971362c4b816ea025f1e7d1e136f8dc3d78
EmmanuelMOUMBOUILOU <emmanuelmoumbouilou@gmail.com>
2025-03-20 17:24:01 +0000

    many modification

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -1,62 +1,70 @@
-#include <stdio.h>
 #include "3-calc.h"
 
 /**
- * op_add - returns the sum of a and b
- * @a: the integer
- * @b: the integer
+ * op_add - Returns the sum of two numbers
+ * @a: First number
+ * @b: Second number
  *
- * Return: the sum
+ * Return: Sum of a and b
  */
-int op_add(int a, int b);
+int op_add(int a, int b)
 {
 	return (a + b);
 }
 
 /**
- * op_sub - returns the difference of a and b
- * @a: the integer
- * @b: the integer
+ * op_sub - Returns the difference of two numbers
+ * @a: First number
+ * @b: Second number
  *
- * Return: the difference
+ * Return: Difference of a and b
  */
-int op_sub(int a, int b);
+int op_sub(int a, int b)
 {
 	return (a - b);
 }
 
 /**
- * op_mul - returns the product of a and b
- * @a: the integer
- * @b: the integer
+ * op_mul - Returns the product of two numbers
+ * @a: First number
+ * @b: Second number
  *
- * Return: the product
+ * Return: Product of a and b
  */
-int op_mul(int a, int b);
+int op_mul(int a, int b)
 {
 	return (a * b);
 }
 
 /**
- * op_div - returns the result of the division of a by b
- * @a: the integer
- * @b: the integer
+ * op_div - Returns the quotient of two numbers
+ * @a: First number
+ * @b: Second number
  *
- * Return: the division
+ * Return: Quotient of a and b, or error if division by zero
  */
-int op_div(int a, int b);
+int op_div(int a, int b)
 {
+	if (b == 0)
+	{
+		return (0); /* Error handled in main */
+	}
 	return (a / b);
 }
 
 /**
- * op_mod - returns the remainder of the division of a by b
- * @a: the integer
- * @b: the integer
+ * op_mod - Returns the remainder of division
+ * @a: First number
+ * @b: Second number
  *
- * Return: the reste
+ * Return: Remainder of a divided by b, or error if division by zero
  */
-int op_mod(int a, int b);
+int op_mod(int a, int b)
 {
+	if (b == 0)
+	{
+		return (0); /* Error handled in main */
+	}
 	return (a % b);
 }
+
