b0db426d13fe035448ab75d3bf1a25fb6a609d83
EmmanuelMOUMBOUILOU <emmanuelmoumbouilou@gmail.com>
2025-03-20 17:17:19 +0000

    the main function only.

---
+++ function_pointers/3-main.c
@@ -0,0 +1,45 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point for calculator program
+ * @argc: Number of command-line arguments
+ * @argv: Array of command-line arguments
+ *
+ * Return: 0 on success, or an error code on failure
+ */
+int main(int argc, char *argv[])
+{
+	int num1, num2, result;
+	int (*operation)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+
+	operation = get_op_func(argv[2]);
+
+	if (!operation)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && num2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	result = operation(num1, num2);
+	printf("%d\n", result);
+
+	return (0);
+}
+
