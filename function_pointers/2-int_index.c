db18fdb93c91a3af0c1ba4f21cc4ce2a05c417df
EmmanuelMOUMBOUILOU <emmanuelmoumbouilou@gmail.com>
2025-03-21 14:07:19 +0000

    add od the condition if

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -13,12 +13,15 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	for (i = 0; i < size; i++)
+	if (array && cmp && size > 0)
 	{
-		if (cmp(array[i]) != 0)
-			return (i);
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]) != 0)
+				return (i);
+		}
+
+		return (-1);
+
 	}
-
-	return (-1);
 }
-
