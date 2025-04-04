2d5b38bd79e3e1f67bfe2e4c0d26c34a71bf1b6c
EmmanuelMOUMBOUILOU <emmanuelmoumbouilou@gmail.com>
2025-04-04 16:55:36 +0200

    modification in format betty

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -10,37 +10,37 @@
 
 list_t *add_node_end(list_t **head, const char *str)
 {
-    list_t *new_node, *temp;
-    unsigned int len = 0;
+	list_t *new_node, *temp;
+	unsigned int len = 0;
 
-    while (str[len])
-        len++;
+	while (str[len])
+		len++;
 
-    new_node = malloc(sizeof(list_t));
-    if (new_node == NULL)
-        return (NULL);
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
 
-    new_node->str = strdup(str);
-    
-    if (new_node->str == NULL)
-    {
-        free(new_node);
-        return (NULL);
-    }
+	new_node->str = strdup(str);
 
-    new_node->len = len;
-    new_node->next = NULL;
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
 
-    if (*head == NULL)
-    {
-        *head = new_node;
-        return (new_node);
-    }
+	new_node->len = len;
+	new_node->next = NULL;
 
-    temp = *head;
-    while (temp->next != NULL)
-        temp = temp->next;
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (new_node);
+	}
 
-    temp->next = new_node;
-    return (new_node);
+	temp = *head;
+	while (temp->next != NULL)
+		temp = temp->next;
+
+	temp->next = new_node;
+	return (new_node);
 }
--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -5,8 +5,9 @@
 #include <stdlib.h>
 #include <string.h>
 
-/* struct list_s - singly linked list
- * @str: string - (malloc'ed string)
+/**
+ * struct list_s - singly linked list
+ * @str: string  (malloc'ed string)
  * @len: length of the string
  * @next: points to the next node
  *
@@ -15,11 +16,10 @@
 
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
-
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
