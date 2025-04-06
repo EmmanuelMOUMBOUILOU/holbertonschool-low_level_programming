a291f648b2b8de5241637e06de1d4cbf401cefc8
EmmanuelMOUMBOUILOU <emmanuelmoumbouilou@gmail.com>
2025-04-06 21:57:56 +0200

    this function Prints error message and exits with a specific code

---
+++ file_io/3-cp.c
@@ -0,0 +1,65 @@
+#include "main.h"
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#define BUF_SIZE 1024
+
+/**
+ * print_error - Prints error message and exits with a specific code
+ * @code: exit code
+ * @msg: error message
+ * @arg: argument for message
+ */
+void print_error(int code, const char *msg, const char *arg)
+{
+	dprintf(STDERR_FILENO, msg, arg);
+	exit(code);
+}
+
+/**
+ * main - Copies the content of a file to another
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0 on success, exits on failure
+ */
+int main(int argc, char *argv[])
+{
+	int fd_from, fd_to, r, w;
+	char buf[BUF_SIZE];
+
+	if (argc != 3)
+		print_error(97, "Usage: %s file_from file_to\n", argv[0]);
+
+	fd_from = open(argv[1], O_RDONLY);
+	if (fd_from == -1)
+		print_error(98, "Error: Can't read from file %s\n", argv[1]);
+
+	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (fd_to == -1)
+	{
+		close(fd_from);
+		print_error(99, "Error: Can't write to %s\n", argv[2]);
+	}
+
+	while ((r = read(fd_from, buf, BUF_SIZE)) > 0)
+	{
+		w = write(fd_to, buf, r);
+		if (w != r)
+		{
+			close(fd_from);
+			close(fd_to);
+			print_error(99, "Error: Can't write to %s\n", argv[2]);
+		}
+	}
+	if (r == -1)
+		print_error(98, "Error: Can't read from file %s\n", argv[1]);
+
+	if (close(fd_from) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from), exit(100);
+	if (close(fd_to) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to), exit(100);
+
+	return (0);
+}
